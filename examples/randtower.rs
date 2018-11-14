7688ef702e216cad0ab3918c6dab3f7b15cd9645
Payton Turnage <paytonturnage@gmail.com>
2018-11-13 22:17:45 -0800

    Fix randtower example.

--- examples/randtower.rs
+++ examples/randtower.rs
@@ -7,22 +7,19 @@ struct RandCube;
 
 impl ToRule for RandCube {
     fn to_rule(&self) -> Rule {
-        cube().tf(*thread_rng()
-            .choose(&[
-                Translate::x(0.1),
-                Translate::x(-0.1),
-                Translate::x(0.2),
-                Translate::x(-0.2),
-            ])
-            .unwrap())
+        Rule::new().push(
+            *thread_rng()
+                .choose(&[Tf::tx(0.1), Tf::tx(-0.1), Tf::tx(0.2), Tf::tx(-0.2)])
+                .unwrap(),
+            cube(),
+        )
     }
 }
 
 fn main() {
+    let meshes = Rule::new()
+        .push(Replicate::n(4, Tf::ty(1.0)), RandCube {})
+        .generate();
     let mut output = File::create("randtower.obj").expect("obj file");
-    generate(
-        Rule::from(RandCube {}).tf(Replicate::n(4, Translate::y(1.0))),
-        &mut output,
-    )
-    .expect("rendered scene");
+    write_meshes(meshes, &mut output).expect("rendered scene");
 }
